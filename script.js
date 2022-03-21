d81f4692307194d9d845e7bc326be51603259656
yahacoral <yahairavittoria@gmail.com>
2022-03-21 17:53:18 -0500

    validate login form inputs

--- script.js
+++ script.js
@@ -282,3 +282,37 @@ const openTab = (evt, tabName) => {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " w3-red";
   }
+
+  //Validate inputs of login form
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+  const form = document.getElementById('validarDatos')
+  const parrafo = document.getElementById('warnings')
+
+  form.addEventListener("submit", e=>{
+      e.preventDefault()
+      let warnings = ""
+      let entrar = false
+      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+      parrafo.innerHTML = ""
+
+    //   if(nombre.value.length < 6) {
+    //       warnings += `El nombre no es valido <br>`
+    //       entrar = true
+    //   }
+
+      if(!regexEmail.test(email.value)){
+          warnings += `El email no es válido. <br>`
+          entrar = true
+      }
+      if(password.value.length < 8){
+          warnings += `La contraseña debe contener mínimo 8 caracteres.`
+          entrar = true
+      }
+      if (entrar) {
+          parrafo.innerHTML = warnings
+        }
+    //   else {
+    //       parrafo.innerHTML = "Enviado"
+    //   }
+  })
